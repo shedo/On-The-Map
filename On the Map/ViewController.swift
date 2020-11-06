4d6e6bdc28ba828d125e9a15b96787c26c0c1a4c
Ivan Zandona <i.zandona@reply.it>
2020-11-06 07:13:53 +0100

    Initial Commit

---
+++ On the Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Ivan Zandonà on 06/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
