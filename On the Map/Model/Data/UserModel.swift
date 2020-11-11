d45a2a5a005565186d78f9920512059a7e50309b
Ivan Zandona <i.zandona@reply.it>
2020-11-11 17:31:11 +0100

    Add missing file

---
+++ On the Map/Model/Data/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  On the Map
+//
+//  Created by Ivan Zandonà on 07/11/2020.
+//
+
+import Foundation
+
+class UserModel {
+    static var lastName = String()
+    static var firstName = String()
+}
